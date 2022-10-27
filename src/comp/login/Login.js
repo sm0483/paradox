5c2a92a17a9e10899ff26efa32b877f6a428faee
sreerag <msratofficial@gmail.com>
2022-10-27 14:01:47 +0530

    feat: create login page and register page and add css

--- src/comp/login/Login.js
+++ src/comp/login/Login.js
@@ -1,7 +1,60 @@
+import login from '../../assets/login.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './login.css'
+import { Link } from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc'
+import {TfiTwitter} from 'react-icons/tfi'
 const Login = () => {
     return ( 
-        <section className="login">
-            <h1>Login</h1>
+        <section className="login container-fluid">
+            <div className="login-page row">
+                <div className="img-container  bg-danger col-3">
+                    <img  src={login} alt="login" className='img-fluid' />
+                </div>
+                <div className="login-input col-7">
+                    <div className="input-container">
+                        <div className="input-head ">
+                            <h3>Sign in to Paradox</h3>
+                            <div className="button-container">
+                                <button className="btn btn-google"
+                                >
+                                    <span className='btnIcon'><FcGoogle/></span>
+                                    <span className='btnText'>Sign in with Google</span>
+                                </button>
+                                <button className=" btn btn-twitter">
+                                    <TfiTwitter/>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="underline text-center"></div>
+                        <div className="cred-container">
+                            <form className='form-control'>
+                                <div className="mb-3 single-input">
+                                    <label for="email" className="form-label">Email</label>
+                                    <input type="email" className="form-control" id="email" 
+                                    placeholder="name@example.com"/>
+                                </div>
+                                <div className="mb-3 single-input">
+                                    <label for="password" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="password" 
+                                    placeholder="@333test"/>
+
+                                    <div id="forget-password" className="form-text">
+                                        <Link to="/reset">Forget password</Link>
+                                    </div>
+
+                                </div>
+                                <button className='btn btn-primary login-button'>
+                                    Sign In
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className="navigation  col-2 text-end">
+                    <span>Not a member?<Link to="/register">Sign up</Link></span>
+                </div>
+            </div>
         </section>   
      );
 }
--- src/comp/register/Register.js
+++ src/comp/register/Register.js
@@ -1,9 +1,57 @@
+import register from '../../assets/register.jpg'
+import './register.css'
+import { Link } from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc'
+import {TfiTwitter} from 'react-icons/tfi'
 const Register = () => {
     return ( 
-        <section className="register">
-            <h1>Register</h1>
-        </section>
-    );
+        <section className="register container-fluid">
+            <div className="register-page row">
+                <div className="img-container  bg-danger col-3">
+                    <img  src={register} alt="register" className='img-fluid' />
+                </div>
+                <div className="register-input col-7">
+                    <div className="input-container">
+                        <div className="input-head ">
+                            <h3>Sign up to Paradox</h3>
+                            <div className="button-container">
+                                <button className="btn btn-google"
+                                >
+                                    <span className='btnIcon'><FcGoogle/></span>
+                                    <span className='btnText'>Sign in with Google</span>
+                                </button>
+                                <button className=" btn btn-twitter">
+                                    <TfiTwitter/>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="underline text-center"></div>
+                        <div className="cred-container">
+                            <form className='form-control'>
+                                <div className="mb-3 single-input">
+                                    <label for="email" className="form-label">Email</label>
+                                    <input type="email" className="form-control" id="email" 
+                                    placeholder="name@example.com"/>
+                                </div>
+                                <div className="mb-3 single-input">
+                                    <label for="password" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="password" 
+                                    placeholder="@333test"/>
+
+                                </div>
+                                <button className='btn btn-primary register-button'>
+                                    Create new account
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className="navigation  col-2 text-end">
+                    <span>Already a member?<Link to="/">Sign In</Link></span>
+                </div>
+            </div>
+        </section>   
+     );
 }
  
 export default Register;
